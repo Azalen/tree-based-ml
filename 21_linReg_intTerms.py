a023315031a18dc08e9b8ca0525b0e6b59b9a106
azalen <azalen@azalen.localdomain>
2024-03-18 15:23:36 +0100

    added graphs for showing variable interactions

--- 21_linReg_intTerms.py
+++ 21_linReg_intTerms.py
@@ -17,7 +17,7 @@ plt.rcParams["figure.figsize"] = (16, 8)
 plt.rcParams['figure.dpi'] = 300
 
 # Laden der Daten
-df = pd.read_csv('filtered_dataset.csv')
+df = pd.read_csv('dataset_v1.csv')
 
 # Aufteilen der Daten in Features (X) und Zielvariable (Y)
 X = df.drop(['Y', 'funcCalls', 'assignm'], axis=1)
@@ -25,45 +25,70 @@ y = df['Y']
 
 # Intervall der X-Achse für schöne Darstellung
 filtered_df = df[df['yLeafs'] <= 100]
-# filtered_df = filtered_df[filtered_df['leafsInLists'] <= 1500]
 
-# 2d scatter plot
-# sns.lmplot(x="leafsInLists", y="Y", hue="spUsed", data=filtered_df, fit_reg=False)
-# plt.ylabel("runtime [µs]")
-# plt.xlabel("Leafs in Lists");
+# 2d scatter plot no hue
+sns.lmplot(x="yLeafs", y="Y", data=filtered_df, fit_reg=False, aspect=16/9, height=4)
+plt.ylabel("runtime [µs]")
+plt.xlabel("yLeafs");
+plt.savefig("22_yLeafs-Y_noHue.png", bbox_inches='tight')
 
-# # HuePlot abspeichern und anzeigen
-# plt.savefig("22_spUsed_interaction.png")
-# plt.show()
+# 2d scatter plot spUsed hue
+sns.lmplot(x="yLeafs", y="Y", hue="spUsed", data=filtered_df, fit_reg=False, aspect=16/9, height=4)
+plt.ylabel("runtime [µs]")
+plt.xlabel("yLeafs");
+plt.savefig("23_yLeafs-Y_spusedHue.png", bbox_inches='tight')
+
+
+
 
 model_1 = smf.ols(formula="Y ~ yLeafs + spUsed", data=filtered_df).fit()
 print(model_1.summary())
-
-# Access the coefficients
 coefficients = model_1.params
 
-# Print the coefficients
-print(coefficients)
-
 X = np.linspace(1, 100, num=20)
-sns.lmplot(x="yLeafs", y="Y", hue="spUsed", data=filtered_df, fit_reg=False)
-plt.title("Best fit lines for from the model without interactions")
+sns.lmplot(x="yLeafs", y="Y", data=filtered_df, fit_reg=True, ci=None, aspect=16/9, height=4)
 plt.ylabel("runtime [µs]")
-plt.xlabel("Vehicle Weight")
-plt.plot(X, (coefficients["Intercept"] + coefficients["spUsed"]) + coefficients["yLeafs"] * X, "blue")
-plt.show()
+plt.xlabel("yLeafs")
+
+
+
+plt.savefig("24_yLeafs-Y_fitLine.png", bbox_inches='tight')
+
+
+
 
 model_2 = smf.ols(formula="Y ~ yLeafs + spUsed + yLeafs:spUsed", data=filtered_df).fit()
 print(model_2.summary())
-# Access the coefficients
 coefficients = model_2.params
 
 X = np.linspace(1, 100, num=20)
-sns.lmplot(x="yLeafs", y="Y", hue="spUsed", data=filtered_df, fit_reg=False)
-plt.title("Best fit lines for from the model with interactions")
+sns.lmplot(x="yLeafs", y="Y", hue="spUsed", data=filtered_df, ci=None, fit_reg=False, aspect=16/9, height=4)
 plt.ylabel("runtime [µs]")
 plt.xlabel("Vehicle Weight")
 plt.plot(X, coefficients["Intercept"] + coefficients["yLeafs"] * X, "blue")
 plt.plot(X, (coefficients["Intercept"] + coefficients["spUsed"]) + (coefficients["yLeafs"] + coefficients["yLeafs:spUsed"]) * X, "orange");
-plt.show()
+plt.savefig("25_yLeafs-Y_spusedHue_fitLine.png", bbox_inches='tight')
 
+
+# 2d scatter plot no hue
+sns.lmplot(x="spUsed", y="Y", data=filtered_df, fit_reg=False, aspect=16/9, height=4)
+plt.ylabel("runtime [µs]")
+plt.xlabel("spUsed");
+
+# Set the x-axis to show only 0 and 1
+plt.xticks([0, 1])
+# Specify the total X-range
+plt.xlim(-0.5, 1.5)
+
+plt.savefig("26_spUsed-Y.png", bbox_inches='tight')
+
+subset0_df = filtered_df[filtered_df['spUsed'] == 0]
+subset1_df = filtered_df[filtered_df['spUsed'] == 1]
+
+Y_mean_sp0 = subset0_df['Y'].mean()
+Y_mean_sp1 = subset1_df['Y'].mean()
+print(round(Y_mean_sp0,2), round(Y_mean_sp1,2))
+plt.hlines(Y_mean_sp0, xmin=-0.25, xmax=0.25, colors='red', linestyles='--')
+plt.hlines(Y_mean_sp1, xmin=0.75, xmax=1.25, colors='red', linestyles='--') 
+
+plt.savefig("27_spUsed-Y_withAver.png", bbox_inches='tight')
